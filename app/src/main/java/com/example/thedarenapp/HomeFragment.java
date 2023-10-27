1cb1165256ecc1562b5c41ce38996107483125fc
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-27 15:03:32 -0400

    :bug: still broken

--- app/src/main/java/com/example/thedarenapp/HomeFragment.java
+++ app/src/main/java/com/example/thedarenapp/HomeFragment.java
@@ -2,7 +2,10 @@ package com.example.thedarenapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -12,6 +15,12 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
     public HomeFragment() {
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NavController navController = Navigation.findNavController(view);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
