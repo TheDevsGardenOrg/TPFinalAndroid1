c9720d84e2efd2ca67f02269033d237b7dbffbb9
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-27 18:39:26 -0400

    :fire: binding

--- app/src/main/java/com/example/thedarenapp/MainActivity.java
+++ app/src/main/java/com/example/thedarenapp/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.thedarenapp;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 
+import com.example.thedarenapp.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+//        setContentView(R.layout.activity_main); //this is old way, we want binding everywhere
+         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 }
