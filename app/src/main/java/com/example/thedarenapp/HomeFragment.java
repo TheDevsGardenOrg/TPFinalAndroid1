4d3d6a5b728b0e979d7e5f5629c39ac1a23f4e90
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-27 14:44:55 -0400

    :bug: navigation nullpointer exception

--- app/src/main/java/com/example/thedarenapp/HomeFragment.java
+++ app/src/main/java/com/example/thedarenapp/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.thedarenapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,12 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {
-        super(R.layout.fragment_home);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+     View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_loginFragment);
+        return view;
     }
 }
--- app/src/main/java/com/example/thedarenapp/MainActivity.java
+++ app/src/main/java/com/example/thedarenapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.thedarenapp;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 
 public class MainActivity extends AppCompatActivity {
