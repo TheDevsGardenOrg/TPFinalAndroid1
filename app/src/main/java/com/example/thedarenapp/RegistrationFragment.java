354836b1160b49bccfb7687003215a4949146180
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-27 20:42:56 -0400

    :bug: ya de quoi qu<il aime pas avec registration

--- app/src/main/java/com/example/thedarenapp/RegistrationFragment.java
+++ app/src/main/java/com/example/thedarenapp/RegistrationFragment.java
@@ -1,8 +1,26 @@
 package com.example.thedarenapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RegistrationFragment extends Fragment {
+
+    public RegistrationFragment() {
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i("RegistrationFragment", "onViewCreated");
+
+    }
+
+
 }
